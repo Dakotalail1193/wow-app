1e99de9ba23d7c73765eb040a0b636b1f69fb3e5
Dakota Lail <dakotalail1193@gmail.com>
2024-10-21 19:55:03 -0400

    server

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = express()
 const morgan = require ('morgan')
 require('dotenv').config()
 const mongoose = require('mongoose')
+const path = require('path')
 
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, "client", "dist")))
 
 async function connectToDb(){
     try {
@@ -27,6 +29,7 @@ app.use ((err, req, res, next) => {
     
 })
 
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "client", "dist", "index.html")))
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
