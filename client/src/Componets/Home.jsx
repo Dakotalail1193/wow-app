1fe3ad0137e7339ce1ea3a080f18ae6476d4ddea
Dakota Lail <dakotalail1193@gmail.com>
2024-10-21 20:23:19 -0400

    test

--- client/src/Componets/Home.jsx
+++ client/src/Componets/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
         <div className='selection'>
           <Link to="/Horde" className="nav">
               <button className='horde-button'>
-                <img src="/src/Assets/Horde_Crest.png" className='crest'/>
+                <img src="../src/Assets/Horde_crest.png" className='crest'/>
               </button>
               <p className="horde-select">Horde</p>  
           </Link>
